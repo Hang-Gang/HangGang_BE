5ac60c804346bb07bd9926f828f414eafc657075
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-16 00:03:03 -0600

    set up knexfile, production needs finished

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,19 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/hg.sqlite3'
+    },
+    useNullAsDefault: true,
+    pool: {
+        afterCreate: (conn, done) => {
+            conn.run('PRAGMA foreign_keys = ON', done);
+        },
+    },
+    migrations:{
+        directory: './data/migrations'
+    },
+    seeds:{
+        directory: './data/seeds'
     }
   },
 
@@ -26,7 +38,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
+    useNullAsDefault: true,
     connection: {
       database: 'my_db',
       user:     'username',
@@ -37,7 +50,7 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
     }
   }
 
